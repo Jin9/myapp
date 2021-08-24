b1b17351973f3b0ba823fca89efacc6907cda2f3
Chinnawat Wongwises <chinnawat.w@infinitaskt.com>
2021-08-24 12:19:53 +0700

    Initial Commit

---
+++ myapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myapp
+//
+//  Created by KTB_User on 24/8/2564 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
