869b76b7fd8e2ba1815b377bf34d06ed26737829
Chinnawat Wongwises <chinnawat.w@infinitaskt.com>
2021-08-26 17:52:31 +0700

    add example click button

--- myapp/ViewController.swift
+++ myapp/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    
+    var isClose = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func onButtonClick(_ sender: Any) {
+        if !isClose {
+            view.backgroundColor = .black
+            myLabel.text = "Hello World"
+            myLabel.textColor = .white
+            isClose = !isClose
+        } else {
+            view.backgroundColor = .white
+            myLabel.text = "Welcome"
+            myLabel.textColor = .black
+            isClose = !isClose
+        }
+    }
 }
 
